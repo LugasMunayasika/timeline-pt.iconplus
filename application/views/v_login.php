dde7a547c2b5498384c8986c64b556aabeaa68ee
robbihably1 <70518249+robbihably1@users.noreply.github.com>
2021-10-06 11:13:21 +0700

    gajadi

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -49,7 +49,7 @@
                 <input type="password" class="form-control" name="password" placeholder="Password" />
               </div>
               <div>
-                <!-- <input type="submit" name="login" class="btn btn-default submit pull-right" value="Login" /> -->
+                <input type="submit" name="login" class="btn btn-default submit pull-right" value="Login" />
               </div>
 
               <div class="clearfix"></div>
